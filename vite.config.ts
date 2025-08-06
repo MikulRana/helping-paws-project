49b7b707067163d645b44647dd04c6ecf3f04b66
Mikul Rana <ranamikul@gmail.com>
2025-08-05 19:51:55 -0700

    pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/helping-paws-project/' : '/',
   server: {
-    host: "::",
-    port: 8080,
+    host: "localhost",
+    port: 3000,
   },
   plugins: [
     react(),
